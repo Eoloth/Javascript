25e287a0fba0b0c6794c97039e548ff6dd239753
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-25 17:11:58 -0400

    Constante

---
+++ Hola-Mundo/JS/04-constantes.js
@@ -0,0 +1,11 @@
+'use strict'
+// CONSTANTES
+// Como una variable, pero su valor no puede cambiar
+
+var web = "https://www.google.es";
+const ip = "192.168.0.1";
+
+web = "https://www.youtube.com";
+// No es posible cambiar el valor de la constante
+
+console.log(web, ip);
