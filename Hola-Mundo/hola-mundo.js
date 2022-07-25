5b4190064ec6c17ccafc794ac40eb5a55a3321d4
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-25 16:24:37 -0400

    Console interaction

--- Hola-Mundo/hola-mundo.js
+++ Hola-Mundo/hola-mundo.js
@@ -1,3 +1,5 @@
 alert("Hola Mundo");
 alert("Bienvenido a mi curso de Javascript");
 document.write("Hola Mundo desde fichero externo");
+console.log("Muestra esto en la consola");
+console.log(88+4);
