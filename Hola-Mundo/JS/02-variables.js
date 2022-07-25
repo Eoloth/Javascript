46525d1a6cb1d8399b50db1b20450f79506b1a94
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-25 16:39:30 -0400

    Variables

---
+++ Hola-Mundo/JS/02-variables.js
@@ -0,0 +1,16 @@
+// VARIABLES
+// Una varibale es un contenedor de información
+
+var pais = "Chile";
+var continente = "South America";
+var antiguedad = 2022;
+var pais_y_continente = pais + ' ' + continente;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
+
+pais = "Mexico";
+continente = "North America";
+var antiguedad = 1985;
+
+console.log(pais, continente, antiguedad);
