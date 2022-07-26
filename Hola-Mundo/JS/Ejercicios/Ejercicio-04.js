4f0d5342940a779ef74e579728514960529bdfa4
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 11:53:34 -0400

    Correccion ejercicio 4 - bucle while agregado, for eliminado. impresion de sentencia indicando que es par

--- Hola-Mundo/JS/Ejercicios/Ejercicio-04.js
+++ Hola-Mundo/JS/Ejercicios/Ejercicio-04.js
@@ -2,5 +2,16 @@
 
 /*
 Ejercicio 4
-
+Mostrar todos los numeros impares que hay entre dos numeros que introduzcamos por teclado.
  */
+
+var numero1 = parseInt(prompt("Introduce el primer numero: ", 0));
+var numero2 = parseInt(prompt("Introduce el segundo numero: ", 0));
+
+while (numero1 < numero2) {
+  numero1++;
+  if (numero1 % 2 !== 0) {
+    console.log("El número " + numero1 + " es impar");
+    document.write("El número " + numero1 + " es impar", "<br>");
+  }
+}
