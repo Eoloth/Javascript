417da5b25507caa86083ef713c687cb48705394f
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 11:14:31 -0400

    BREAK

--- Hola-Mundo/JS/10-do-while.js
+++ Hola-Mundo/JS/10-do-while.js
@@ -2,15 +2,19 @@
 
 var year = 2018;
 
-while (year != 1991) {
+while (year !== 1991) {
   console.log("Estamos en el año: " + year);
+  if (year === 2000) {
+    break;
+  }
   year--;
 }
 
 // Do while
 
-var years = 20;
+var years = 30;
 
 do {
   alert("Solo cuando sea diferente de 20");
-} while (years !== 20);
+  years--;
+} while (years > 25);
