72bed5cc89fdc4f5798e619a12698805b12a66d4
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 10:58:58 -0400

    Bucle FOR

---
+++ Hola-Mundo/JS/08-blucle-for.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Blucle for
+// Bucle es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for (var i = 0; i < numero; i++) {
+    console.log("Número: " + i);
+    debugger;
+}
