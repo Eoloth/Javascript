20fd0daef81be374e055dd2677a16439a8fc80d7
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 13:16:44 -0400

    Funciones

---
+++ Hola-Mundo/JS/12-funciones.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Funciones
+// Una función es un conjunto de instrucciones que se ejecutan cuando se llama a la función.
+// Las funciones pueden recibir parámetros y devolver valores.
+// Las funciones pueden ser declaradas antes de ser utilizadas.
+// Las funciones pueden ser declaradas dentro de una función.
+
+function calculadora() {
+  console.log("Hola soy la calculadora");
+  console.log("si soy yo");
+
+  // return "Hola soy la calculadora"; PERMITE RETORNAR FUNCIONES
+  //console.log o return, no usar ambas a la vez
+}
+
+// Invocar o llamar a la funcion
+calculadora();
