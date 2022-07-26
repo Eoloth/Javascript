ed58d69aa87a4f6e5b334bafe40fbf80e935d21f
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 17:15:25 -0400

    Arrays básicos

---
+++ Hola-Mundo/JS/22-arrays.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Array, Arreglo, Vectores o Matrices
+// Un array es una colección de datos ordenados y indexados.
+// Los arrays en JavaScript son objetos.
+// Los arrays en JavaScript son dinamicos.
+// Los arrays en JavaScript son mutables.
+
+var nombre = ["Jorge", 52, true, "Hola"];
+
+console.log(nombre);
+
+// Cada array tiene un indice, comenzando en cero
+var lenguajes = new Array("PHP", "JS", "Java", "C++");
+
+console.log(lenguajes[2]);
