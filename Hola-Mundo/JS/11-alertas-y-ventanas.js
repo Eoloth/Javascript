e5c6c0e10af79d456d1e1874d39d23a2d4ec4373
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 11:23:27 -0400

    Alertas y Ventanas

---
+++ Hola-Mundo/JS/11-alertas-y-ventanas.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+// ALERTA
+alert("Esta es mi alerta !! ");
+alert("Este es mi texto");
+
+// CONFIRMACION
+// almacenar confimacion de usuario con variable
+var mi_resultado = confirm("Esta es mi confirmacion");
+console.log(mi_resultado);
+
+*/
+
+// INGRESO DE DATOS DEL USUARIO USANDO UNA VENTANA EMERGENTE
+var mi_resultado = prompt("¿Que edad tienes?", 18);
+console.log(typeof mi_resultado);
+console.log(mi_resultado);
