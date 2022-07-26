53f34b9d984a3be152cef8a79a6f25fcf7aee588
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 11:45:20 -0400

    Correccion ejercicio 03: print de numeros en index.html

--- Hola-Mundo/JS/Ejercicios/Ejercicio-03.js
+++ Hola-Mundo/JS/Ejercicios/Ejercicio-03.js
@@ -10,5 +10,6 @@ var numero1 = prompt("Introduce el primer número", 0);
 var numero2 = prompt("Introduce el segundo número", 0);
 
 for (var i = numero1; i <= numero2; i++) {
+  document.write(i + "<br>");
   console.log(i);
 }
---
+++ Hola-Mundo/JS/Ejercicios/Ejercicio-04.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/*
+Ejercicio 4
+
+ */
