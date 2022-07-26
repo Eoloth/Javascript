93b97ede8cacfa2c03ed3c7ca082b3f73a91ebdc
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 10:43:35 -0400

    Operadores Lógicos

--- Hola-Mundo/JS/06-condicionales.js
+++ Hola-Mundo/JS/06-condicionales.js
@@ -40,3 +40,31 @@ if (edad >= 18)
     console.log(nombre + " tiene " + edad + " años, es menor de edad"); // Ruben tiene 18 años, es menor de edad
 }
 
+/*
+// Operadores lógicos
+    and: &&
+    or: ||
+    not: !
+ */
+
+var year = 2018;
+
+// Negación / not
+if( year !== 2016){
+    console.log("El año no es 2016");
+}
+
+// AND
+if( year === 2016 && year > 2000){
+    console.log("El año es 2016 y es mayor a 2000");
+}
+
+// OR
+if( year === 2016 || (year >= 2018 && year === 2028)){
+    console.log("El año es 2016 o es mayor a 2000");
+}else{
+    console.log("Año no registrado");
+}
+
+
+
