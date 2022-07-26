c9c7cb338d377043d5eefaf1f0ecf1313843d51b
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-26 12:55:59 -0400

    Update ejercicio 7 con document.write

--- Hola-Mundo/JS/Ejercicios/Ejercicio-07.js
+++ Hola-Mundo/JS/Ejercicios/Ejercicio-07.js
@@ -7,6 +7,8 @@
 
 var numero = parseInt(prompt("Ingrese un número: "));
 
+document.write("<h1>Tabla de multiplicar del número " + numero + "</h1>");
 for (var i = 1; i <= 10; i++) {
   console.log(numero + " x " + i + " = " + numero * i);
+  document.write(numero + " x " + i + " = " + numero * i + "<br>");
 }
