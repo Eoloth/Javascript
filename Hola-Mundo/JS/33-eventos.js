5348feb27d415a0bbc43aa666b23fd1585f39135
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-27 17:10:25 -0400

    Load

--- Hola-Mundo/JS/33-eventos.js
+++ Hola-Mundo/JS/33-eventos.js
@@ -2,64 +2,69 @@
 
 // Eventos del mouse
 
-function cambiarColor() {
-  console.log("Me has dado click");
-  var bg = boton.style.background;
+// cuando la pagina este cargada, estos elementos estaran disponibles para usar
+// permite cambiar orden de carga de index.html
 
-  if (bg === "green") {
-    boton.style.background = "red";
-  } else {
-    boton.style.background = "green";
+window.addEventListener("load", () => {
+  function cambiarColor() {
+    console.log("Me has dado click");
+    var bg = boton.style.background;
+
+    if (bg === "green") {
+      boton.style.background = "red";
+    } else {
+      boton.style.background = "green";
+    }
+    boton.style.padding = "10px";
+    boton.style.border = "1px solid #ccc";
+
+    return true;
   }
-  boton.style.padding = "10px";
-  boton.style.border = "1px solid #ccc";
 
-  return true;
-}
+  var boton = document.querySelector("#boton");
 
-var boton = document.querySelector("#boton");
+  // Click
+  boton.addEventListener("click", function () {
+    cambiarColor();
+  });
 
-// Click
-boton.addEventListener("click", function () {
-  cambiarColor();
-});
+  // Mouse over
+  boton.addEventListener("mouseover", function () {
+    boton.style.background = "#ccc";
+  });
 
-// Mouse over
-boton.addEventListener("mouseover", function () {
-  boton.style.background = "#ccc";
-});
+  // Mouseout
+  boton.addEventListener("mouseout", function () {
+    boton.style.background = "#999";
+  });
 
-// Mouseout
-boton.addEventListener("mouseout", function () {
-  boton.style.background = "#999";
-});
+  // Focus
+  var input = document.querySelector("#campo_nombre");
 
-// Focus
-var input = document.querySelector("#campo_nombre");
+  input.addEventListener("focus", function () {
+    console.log("Estas dentro del input: focus");
+  });
 
-input.addEventListener("focus", function () {
-  console.log("Estas dentro del input: focus");
-});
+  // Blur
+  input.addEventListener("blur", function () {
+    console.log("Estas fuera del input: blur");
+  });
 
-// Blur
-input.addEventListener("blur", function () {
-  console.log("Estas fuera del input: blur");
-});
+  // Keydown
+  input.addEventListener("keydown", function () {
+    console.log(
+      "Estas pulsando esta tecla: ",
+      String.fromCharCode(event.keyCode)
+    );
+  });
 
-// Keydown
-input.addEventListener("keydown", function () {
-  console.log(
-    "Estas pulsando esta tecla: ",
-    String.fromCharCode(event.keyCode)
-  );
-});
+  // Keypress
+  input.addEventListener("keypress", function () {
+    console.log("Tecla presionada: ", String.fromCharCode(event.keyCode));
+  });
 
-// Keypress
-input.addEventListener("keypress", function () {
-  console.log("Tecla presionada: ", String.fromCharCode(event.keyCode));
-});
-
-// Keyup
-input.addEventListener("keyup", function () {
-  console.log("Tecla ya pulsada: ", String.fromCharCode(event.keyCode));
+  // Keyup
+  input.addEventListener("keyup", function () {
+    console.log("Tecla ya pulsada: ", String.fromCharCode(event.keyCode));
+  });
 });
