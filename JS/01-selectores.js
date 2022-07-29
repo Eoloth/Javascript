3b1a57d97a4bc3dbca54b90c0870ad1ba0a65580
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-29 15:08:09 -0400

    JQuery Selectores

---
+++ JS/01-selectores.js
@@ -0,0 +1,7 @@
+// Jquery(sleector).elemento()
+$(document).ready(function () {
+  // Selector de ID
+  $("#rojo").css("background", "red").css("color", "white"); //fondo rojo letras blancas
+  $("#amarillo").css("background", "yellow").css("color", "green");
+  $("#verde").css("background", "green").css("color", "white");
+});
