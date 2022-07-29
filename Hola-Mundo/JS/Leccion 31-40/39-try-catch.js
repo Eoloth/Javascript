0ed3ee9f5f83099f25d1ce16420ada97a767b2c0
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-29 14:19:52 -0400

    try catch

---
+++ Hola-Mundo/JS/Leccion 31-40/39-try-catch.js
@@ -0,0 +1,10 @@
+"use strict";
+
+try {
+  var year = 21;
+  alert(year);
+  var vector = new Array(9999999999999);
+} catch (error) {
+  console.log(error);
+  console.log("Ha ocurrido un error en el código");
+}
