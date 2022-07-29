d7980e48a3f12b8efe02d6471b54547f03a716a3
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-29 15:18:09 -0400

    Selector de clase

--- JS/01-selectores.js
+++ JS/01-selectores.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
   $("#rojo").css("background", "red").css("color", "white"); //fondo rojo letras blancas
   $("#amarillo").css("background", "yellow").css("color", "green");
   $("#verde").css("background", "green").css("color", "white");
+
+  // Selector de clase
+  var mi_clase = $(".zebra").css("padding", "50px");
+
+  $(".sin_borde").click(function () {
+    console.log("Click dado");
+    $(this).addClass("zebra"); // no funciona porque clase zebra no esta definida
+  });
 });
