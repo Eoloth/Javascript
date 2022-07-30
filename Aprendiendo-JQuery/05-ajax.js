d5cf75e5ffbfae36331faa0ed7c7f87b15fd48be
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-30 18:42:39 -0400

    Metodo POST AJAX

--- Aprendiendo-JQuery/05-ajax.js
+++ Aprendiendo-JQuery/05-ajax.js
@@ -8,4 +8,28 @@ $(document).ready(function () {
       $("#datos").append(`<li>${user.first_name} ${user.last_name}</li>`);
     });
   });
+
+  /*
+  var usuario = {
+    nombre: "Juan",
+    apellido: "Perez",
+  };
+  $.post("https://reqres.in/api/users", usuario, function (response) {
+    console.log(response);
+  });
+
+   */
+  $("#formulario").submit(function (e) {
+    e.preventDefault();
+    var usuario = {
+      nombre: $('input[name="nombre"]').val(),
+      apellido: $('input[name="apellido"]').val(),
+    };
+    $.post($(this).attr("action"), usuario, function (response) {
+      console.log(response);
+    }).done(function () {
+      alert("Usuario añadido");
+    });
+    return false;
+  });
 });
