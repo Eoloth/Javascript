c14c3e0fdf63dcc5e7404f86d6295ab710fc1cf3
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-30 14:40:21 -0400

    MouseOver and MouseOut

---
+++ Aprendiendo-JQuery/02-eventos.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // MouseOver y MouseOut
+  var caja = $("#caja");
+  caja.mouseover(function () {
+    $(this).css("background", "red");
+  });
+  caja.mouseout(function () {
+    $(this).css("background", "blue");
+  });
+});
