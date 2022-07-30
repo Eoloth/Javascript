835d00c1f32fa481960864a529ba78af6d7a3557
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-30 15:11:18 -0400

    MouseUp, MouseDown and MouseMove

--- Aprendiendo-JQuery/02-eventos.js
+++ Aprendiendo-JQuery/02-eventos.js
@@ -29,12 +29,29 @@ $(document).ready(function () {
 
   // Focus y Blur
   var nombre = $("#nombre");
+  var datos = $("#datos");
 
   nombre.focus(function () {
     $(this).css("border", "2px solid green");
   });
   nombre.blur(function () {
     $(this).css("border", "1px solid transparent");
-    $("#datos").text($(this).val()).show();
+    datos.text($(this).val()).show();
+  });
+
+  // MouseDown y MouseUp
+  datos.mousedown(function () {
+    $(this).css("border-color", "gray");
+  });
+  datos.mouseup(function () {
+    $(this).css("border-color", "black");
+  });
+
+  // MouseMove
+  $(document).mousemove(function () {
+    $("body").css("cursor", "none"); //elimina el cursor
+    var sigueme = $("#sigueme");
+    sigueme.css("left", event.clientX).css("top", event.clientY); //sigue al mouse
+    // reemplazo de cursor por bola amarilla
   });
 });
