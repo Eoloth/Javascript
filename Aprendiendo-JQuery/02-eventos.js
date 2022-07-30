156d459d3d88818595be947ffbabf72b56100ad0
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-30 14:45:36 -0400

    Hover

--- Aprendiendo-JQuery/02-eventos.js
+++ Aprendiendo-JQuery/02-eventos.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
   // MouseOver y MouseOut
   var caja = $("#caja");
-  caja.mouseover(function () {
+  /* caja.mouseover(function () {
     $(this).css("background", "red");
   });
   caja.mouseout(function () {
     $(this).css("background", "blue");
   });
+  */
+  // Hover
+
+  function cambiaRojo() {
+    $(this).css("background", "red");
+  }
+  function cambiaVerde() {
+    $(this).css("background", "green");
+  }
+
+  caja.hover(cambiaRojo, cambiaVerde);
 });
