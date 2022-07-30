a134ef37d49bb36840961ed41bd3d932e303c443
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-07-30 18:33:05 -0400

    Metodo GET AJAX

--- Aprendiendo-JQuery/05-ajax.js
+++ Aprendiendo-JQuery/05-ajax.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
   // Load
-  $("#datos").load("https://reqres.in/");
+  // $("#datos").load("https://reqres.in/");
+
+  // Get y Post
+  $.get("https://reqres.in/api/users", { page: 2 }, function (response) {
+    response.data.forEach((user, index) => {
+      $("#datos").append(`<li>${user.first_name} ${user.last_name}</li>`);
+    });
+  });
 });
