133eeca7dab1480d0debe5f0850a65c8e227aa84
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-01 18:28:45 -0400

    Projecto - Acordeón en SOBRE MI

--- Proyecto/js/main.js
+++ Proyecto/js/main.js
@@ -1,91 +1,95 @@
 $(document).ready(function () {
-  // Slider
-  $(".slider").bxSlider({
-    mode: "fade",
-    caption: true,
-    sidewith: 1200,
-  });
+  if (window.location.href.indexOf("index") > -1) {
+    // Slider
+    $(".slider").bxSlider({
+      mode: "fade",
+      caption: true,
+      sidewith: 1200,
+    });
+  }
 
   // Post
-  var posts = [
-    {
-      title: "Prueba de titulo 1",
-      date:
-        "Publicado el día: " +
-        moment().date() +
-        " de " +
-        moment().format("MMMM") +
-        " de " +
-        moment().format("YYYY"),
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
-        "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
-        "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
-        "              quisquam quos repudiandae.",
-    },
-    {
-      title: "Prueba de titulo 2",
-      date:
-        "Publicado el día: " +
-        moment().date() +
-        " de " +
-        moment().format("MMMM") +
-        " de " +
-        moment().format("YYYY"),
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
-        "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
-        "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
-        "              quisquam quos repudiandae.",
-    },
-    {
-      title: "Prueba de titulo 3",
-      date:
-        "Publicado el día: " +
-        moment().date() +
-        " de " +
-        moment().format("MMMM") +
-        " de " +
-        moment().format("YYYY"),
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
-        "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
-        "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
-        "              quisquam quos repudiandae.",
-    },
-    {
-      title: "Prueba de titulo 4",
-      date:
-        "Publicado el día: " +
-        moment().date() +
-        " de " +
-        moment().format("MMMM") +
-        " de " +
-        moment().format("YYYY"),
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
-        "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
-        "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
-        "              quisquam quos repudiandae.",
-    },
-    {
-      title: "Prueba de titulo 5",
-      date:
-        "Publicado el día: " +
-        moment().date() +
-        " de " +
-        moment().format("MMMM") +
-        " de " +
-        moment().format("YYYY"),
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
-        "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
-        "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
-        "              quisquam quos repudiandae.",
-    },
-  ];
-  posts.forEach((item, index) => {
-    var post = `
+  if (window.location.href.indexOf("index") > -1) {
+    var posts = [
+      {
+        title: "Prueba de titulo 1",
+        date:
+          "Publicado el día: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " de " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
+          "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
+          "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
+          "              quisquam quos repudiandae.",
+      },
+      {
+        title: "Prueba de titulo 2",
+        date:
+          "Publicado el día: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " de " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
+          "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
+          "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
+          "              quisquam quos repudiandae.",
+      },
+      {
+        title: "Prueba de titulo 3",
+        date:
+          "Publicado el día: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " de " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
+          "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
+          "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
+          "              quisquam quos repudiandae.",
+      },
+      {
+        title: "Prueba de titulo 4",
+        date:
+          "Publicado el día: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " de " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
+          "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
+          "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
+          "              quisquam quos repudiandae.",
+      },
+      {
+        title: "Prueba de titulo 5",
+        date:
+          "Publicado el día: " +
+          moment().date() +
+          " de " +
+          moment().format("MMMM") +
+          " de " +
+          moment().format("YYYY"),
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci\n" +
+          "              cumque dolorum, enim error eveniet id molestiae nobis pariatur\n" +
+          "              quas veritatis! Aperiam eligendi molestias nobis, non odio quam\n" +
+          "              quisquam quos repudiandae.",
+      },
+    ];
+
+    posts.forEach((item, index) => {
+      var post = `
     <article class="post">
         <h2>${item.title}</h2>
         <span class="date">${item.date}</span>
@@ -93,8 +97,9 @@ $(document).ready(function () {
         <a href="#" class="button-more">Leer Mas</a>
     </article>
   `;
-    $("#posts").append(post);
-  });
+      $("#posts").append(post);
+    });
+  }
 
   // Selector de tema
   var theme = $("#theme");
@@ -138,5 +143,8 @@ $(document).ready(function () {
       localStorage.clear();
       location.reload();
     });
+  }
+  if (window.location.href.indexOf("about") > -1) {
+    $("#acordeon").accordion();
   }
 });
