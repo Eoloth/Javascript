d13cce1b67a547cb5cb2dbf6e321653cc3d264ed
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-02 15:08:23 -0400

    POO y TypeScript - Clases

---
+++ poo-typescript/js/camiseta.ts
@@ -0,0 +1,32 @@
+// Clase (molde del objeto
+class Camiseta {
+  // Propiedades (caracteristicas del objeto)
+  public color: string;
+  public modelo: string;
+  public marca: string;
+  public talla: string;
+  public precio: number;
+  // Metodos (funciones o acciones del objeto)
+  constructor(color, modelo, marca, talla, precio) {
+    this.color = color;
+    this.modelo = modelo;
+    this.marca = marca;
+    this.talla = talla;
+    this.precio = precio;
+  }
+  public setColor(color) {
+    this.color = color;
+  }
+  public getColor() {
+    return this.color;
+  }
+}
+
+// @ts-ignore
+var camiseta = new Camiseta();
+camiseta.setColor("Rojo");
+
+var playera = new Camiseta("Azul", "Manga Corta", "Adidas", "XL", 12);
+
+console.log(camiseta.getColor());
+console.log(playera);
