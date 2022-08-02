9552faf7c3a73bb97153483143b7e45636b26044
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-02 14:02:49 -0400

    POO y Typescript - Tipos de datos

---
+++ poo-typescript/js/03-tipos-de-datos.ts
@@ -0,0 +1,21 @@
+// string
+let cadena: string = "Hola mundo";
+
+// number
+let edad: number = 129;
+
+// boolean
+let mayorDeEdad: boolean = true;
+
+// any
+let cualquierCosa: any = "Hola mundo";
+
+// array
+let array: Array<number> = [1, 2, 3, 4, 5];
+let array2: Array<string> = ["Hola", "mundo"];
+let years: any[] = ["DOCE", 13, "CATORCE", 15, 16];
+
+console.log(cadena, edad, mayorDeEdad, cualquierCosa);
+console.log(array);
+console.log(array2);
+console.log(years);
