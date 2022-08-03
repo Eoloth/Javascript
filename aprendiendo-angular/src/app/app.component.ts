5d27aec056ea14af4f8ff85a33168ace438331e0
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-02 21:25:30 -0400

    Angular - Hooks, eventos de ciclos de vida

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+  public title = 'aprendiendo-angular';
+  public mostar_videojuegos: boolean = true;
+
+  ocultarVideojuegos(value: boolean) {
+    this.mostar_videojuegos = value;
+  }
 }
--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,12 +1,10 @@
-// @ts-ignore
-
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'videojuego',
   templateUrl: './videojuego.component.html',
 })
-export class VideojuegoComponent {
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
   public titulo: string;
   public listado: string;
 
@@ -15,5 +13,21 @@ export class VideojuegoComponent {
     this.listado = 'Listado de los juegos más populares';
 
     console.log('Se ha cargado el componente: videojuego.component.ts');
+  }
+
+  ngOnInit(): void {
+    console.log('Se ha cargado el método: ngOnInit()');
+  }
+
+  ngDoCheck(): void {
+    console.log('Se ha cargado el método: ngDoCheck()');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Se ha cargado el método: ngOnDestroy()');
+  }
+
+  cambiarTitulo(): void {
+    this.titulo = 'Nuevo título';
   }
 }
