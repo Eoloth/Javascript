093e3a87ec013a48960c0bf3d7341cb6070b345c
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-02 20:55:35 -0400

    Angular - Multiples componentes

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @NgModule({
-  declarations: [AppComponent, VideojuegoComponent],
+  declarations: [AppComponent, VideojuegoComponent, ZapatillasComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -4,17 +4,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'videojuego',
-  template: `
-    <h2>Componente de videojuego</h2>
-    <ul>
-      <li>GTA</li>
-      <li>Fifa</li>
-      <li>Mario</li>
-    </ul>
-  `,
+  templateUrl: './videojuego.component.html',
 })
 export class VideojuegoComponent {
+  public titulo: string;
+  public listado: string;
+
   constructor() {
+    this.titulo = 'Componente de videojuegos';
+    this.listado = 'Listado de los juegos más populares';
+
     console.log('Se ha cargado el componente: videojuego.component.ts');
   }
 }
