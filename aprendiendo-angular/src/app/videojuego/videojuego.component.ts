36e5c6cfff1e42aa2acc9932de320e1256b16376
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-02 20:25:36 -0400

    Angular - videojuego Componente

--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,1 +1,20 @@
+// @ts-ignore
+
 import { Component } from '@angular/core';
+
+@Component({
+  selector: 'videojuego',
+  template: `
+    <h2>Componente de videojuego</h2>
+    <ul>
+      <li>GTA</li>
+      <li>Fifa</li>
+      <li>Mario</li>
+    </ul>
+  `,
+})
+export class VideojuegoComponent {
+  constructor() {
+    console.log('Se ha cargado el componente: videojuego.component.ts');
+  }
+}
