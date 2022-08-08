659cf7621809b68353b9278cb9a85342d922d266
Eoloth <53416886+Eoloth@users.noreply.github.com>
2022-08-08 16:48:49 -0400

    Backend - conectando DB con Node

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 "use strict";
+
+var mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost:27017/portafolio")
+  .then(() =>
+    console.log("Conectado a la base de datos establecida satisfactoriamente")
+  )
+  .catch((err) => console.log(err));
