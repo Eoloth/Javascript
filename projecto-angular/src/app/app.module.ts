37066fc2271ed4bc3845e15f0a3088eae4cedb0b
Eoloth <53416886+Eoloth@users.noreply.github.com>
2023-04-01 18:02:48 -0300

    Projecto Angular - Estilos del header

---
+++ projecto-angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent,
+  ],
+  imports: [BrowserModule, routing],
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ projecto-angular/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRouters: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'sobre-mi', component: AboutMeComponent },
+  { path: 'projectos', component: ProjectsComponent },
+  { path: 'crear-projecto', component: CreateComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', component: ErrorComponent }, //ruta 404
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> =
+  RouterModule.forRoot(appRouters);
