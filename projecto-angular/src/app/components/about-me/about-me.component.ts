02d8ed58f6d43fc88f1e44d05b033f2c3be6d31f
Eoloth <53416886+Eoloth@users.noreply.github.com>
2023-04-03 15:55:38 -0400

    Projecto Angular - Estilos y contenido "Sobre-mi.html"

--- projecto-angular/src/app/components/about-me/about-me.component.ts
+++ projecto-angular/src/app/components/about-me/about-me.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.title = 'Rubén Manríquez';
+    this.subtitle = 'Desarrollador web';
+    this.email = 'rms@rms.cl';
   }
 
+  ngOnInit(): void {}
 }
